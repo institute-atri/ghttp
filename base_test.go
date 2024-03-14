c01483af44095d3b1bf6edd06f96bf603e5cc32e
Ygor Simões <contato.ygorsimoes@gmail.com>
2024-03-14 20:40:58 -0300

    fix(tests): delete function test of proxy

--- base_test.go
+++ base_test.go
@@ -19,22 +19,3 @@ func TestGet(t *testing.T) {
 	log.Println(response.BRaw)
 	log.Println("Request Time: " + fmt.Sprint(response.Duration.Seconds()))
 }
-
-func TestGetProxy(t *testing.T) {
-	var request = NewHttp()
-
-	request.SetURL("http://httpbin.org/get")
-	request.SetMethod("GET")
-	request.OnTor()
-
-	var response, err = request.Do()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println(response.BRaw)
-
-	log.Println(CheckConnectionTor())
-	log.Println("Request Time: " + fmt.Sprint(response.Duration.Seconds()))
-}
